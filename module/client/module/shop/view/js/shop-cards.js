f4cff9242fc006e00d6fbf3af0f1034d969aa282
Joel Portatil <jrevertvila@gmail.com>
2020-02-05 22:54:49 +0100

    add data menu card shop effect. FALTA TERMINAR

--- module/client/module/shop/view/js/shop-cards.js
+++ module/client/module/shop/view/js/shop-cards.js
@@ -1,8 +1,38 @@
 $(document).ready(function(){
     $('.info-button').on('click', function() {
         console.log("ssaa");
+        $('.card-shop-img').css({
+            "height": "230px"
+        });
+        $('.card-shop-data').css({
+            "height": "0px"
+        });
+        var card_img = $(this).parent().find('.card-shop-img');
+        console.log(card_img);
+
+        if (!card_img.hasClass('toggled')){
+            card_img.addClass('toggled');
+        }else{
+            card_img.removeClass('toggled');
+        }
+            
 
 
+        
+
+        var card_data = $(this).parent().find('.card-shop-data');
+        console.log(card_data);
+        card_data.css({
+            "height": "230px",
+
+        });
+
+        if (!card_data.hasClass('toggled-data')){
+            card_data.addClass('toggled-data');
+        }else{
+            card_data.removeClass('toggled-data');
+        }
+        
     });
 
     $('.card-shop-img').on('click', function() {
